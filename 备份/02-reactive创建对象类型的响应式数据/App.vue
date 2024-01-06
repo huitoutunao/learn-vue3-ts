fe6bc11ebdabcf6909b3788102949fd756fbe490
huitoutunao <lh_huitoutunao@163.com>
2024-01-06 14:23:03 +0800

    feat: 新增reactive创建对象类型数据

---
+++ 备份/02-reactive创建对象类型的响应式数据/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Person />
+</template>
+
+<script lang="ts">
+import Person from '@/components/Person.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Person,
+  },
+}
+</script>
+
+<style>
+.page {
+  padding: 20px;
+  background-color: #ddd;
+  border-radius: 4px;
+  box-shadow: 0 0 10px;
+}
+</style>
