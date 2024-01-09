1ff7d78b57b22ede392b4cf2de4e46e43fc79b7b
huitoutunao <lh_huitoutunao@163.com>
2024-01-09 14:57:23 +0800

    feat: 新增watch

---
+++ 备份/07-watch/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Person />
+</template>
+
+<script lang="ts">
+import Person from '@/components/Person.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Person,
+  },
+}
+</script>
+
+<style>
+.page {
+  margin: 30px;
+  padding: 20px;
+  background-color: beige;
+  border-radius: 4px;
+  box-shadow: 0 0 10px;
+}
+</style>
