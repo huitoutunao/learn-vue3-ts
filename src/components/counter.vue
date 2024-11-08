4fc2452ad878eb703f46ebe6039d2fa928262822
huitoutunao <lh_huitoutunao@163.com>
2024-11-08 12:01:02 +0800

    feat: 新增template与jsx混合编程

---
+++ src/components/counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="render()"></component>
+</template>
+
+<script setup lang="tsx">
+const props = defineProps<{
+  count: number
+}>()
+
+const emits = defineEmits(['update:count'])
+
+const add = () => {
+  emits('update:count', props.count + 1)
+}
+const render = () => {
+  return (
+    <div>
+      <div class="content">count is {props.count}</div>
+      <el-button type="primary" onClick={add}>加1</el-button>
+    </div>
+  )
+}
+</script>
+
+<style scoped>
+.content {
+  color: #409eff;
+}
+</style>
